c6474f462101d1339f50177bec7dd832cad284c4
Vespri <kresna.vespri21@gmail.com>
2022-11-19 11:11:13 +0700

    pagination done

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(array('url'));
+        $this->load->model('M_data');
+    }
+
+    public function index()
+    {
+        $this->load->database();
+        $jumlah_data = $this->M_data->jumlah_data();
+        $this->load->library('pagination');
+        $config['base_url'] = base_url() . 'index.php/welcome/index/';
+        $config['total_rows'] = $jumlah_data;
+        $config['per_page'] = 5;
+        $from = $this->uri->segment(3);
+        $this->pagination->initialize($config);
+        $data['user'] = $this->M_data->data($config['per_page'], $from);
+        $this->load->view('v_data', $data);
+    }
 }
